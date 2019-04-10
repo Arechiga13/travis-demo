f35838062f12a047b01b359d582c319b9655237c
Manuel Arechiga <manuel.arechiga@amplemind.com>
2019-04-10 13:57:26 -0500

    Update Alert

--- src/app/pwa.service.ts
+++ src/app/pwa.service.ts
@@ -1,29 +1,18 @@
 import { Injectable } from '@angular/core';
-import { MatSnackBar } from '@angular/material';
 import { SwUpdate } from '@angular/service-worker';
 
 @Injectable()
 export class PwaService {
     promptEvent;
-    constructor(private swUpdate: SwUpdate, private snackbar: MatSnackBar) {
-        this.swUpdate.available.subscribe(event => {
-            const snack = this.snackbar.open('Update Available', 'Reload');
-
-            snack
-                .onAction()
-                .subscribe(() => {
-                window.location.reload();
-            });
-
-            // snack.setTimeout(() => {
-            //     snack.dismiss();
-            // }, 6000);
-          });
+    constructor(private swUpdate: SwUpdate) {
         window.addEventListener('beforeinstallprompt', event => {
             this.promptEvent = event;
         });
         window.addEventListener('appinstalled', (event) => {
             alert('App installed!');
-           });
+        });
+        window.addEventListener('appupdated', (event) => {
+            alert('App Update avable!');
+        });
     }
 }
